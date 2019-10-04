c12a530e3549f34925ec0252afc11430686b587c
MarioCesarJr <mcsjuniorsouza@gmail.com>
2019-10-04 14:46:56 -0300

    omnistack 09

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # aircnc
+
 Omnistack 09
+
+## Frontend
+
+![frontend-screenshot01](screenshots/frontend-screenshot01.png)
+
+![frontend-screenshot02](screenshots/frontend-screenshot02.png)
+
+![frontend-screenshot03](screenshots/frontend-screenshot03.png)
+
+## Mobile
+
+![mobile](screenshots/mobile.png)
+
---
+++ mobile/src/pages/List.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import socketio from 'socket.io-client';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  ScrollView,
+  AsyncStorage,
+  Platform,
+  StatusBar,
+  Alert
+} from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://10.0.2.2:3333', {
+        query: { user_id }
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? 'APROVADA' : 'REJEITADA'
+          }`
+        );
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' && StatusBar.currentHeight
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+});
